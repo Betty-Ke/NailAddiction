db71430a3164abe6c80d45c790e2b53250415b24
Betty-Ke <oct19458@gmail.com>
2022-04-22 01:22:15 +0800

    pdtjs csschangename

--- js/index.js
+++ js/index.js
@@ -23,41 +23,87 @@ function dot_click(){
     }
 
 }
-    
 
 
-
-// 購物車數量
-var numplus = document.getElementById("numplus");
-var numcut = document.getElementById("numcut");
-var inputnum = document.getElementById("inputnum");
-
-numplus.addEventListener("click", function(){
-    inputnum.value = parseInt(inputnum.value) + 1;
+// Home cat_link
+document.addEventListener("DOMContentLoaded", function(){
+    var search_obj = new URLSearchParams(location.search);
+    // console.log(search_obj.get("target"));
+    var all = document.getElementsByClassName("pdt_card");
+    var catproduct2 = document.getElementsByClassName("pdt_cat2");
+    var catproduct3 = document.getElementsByClassName("pdt_cat3");
+    var catproduct4 = document.getElementsByClassName("pdt_cat4"); 
+    var cat_btn_now = document.getElementsByClassName('cat_btn_now');
+    switch(search_obj.get("target")){
+        case '3':
+            for(let i = 0; i < all.length; i++){
+            	all[i].style.display = "none";
+            }
+            for(let j = 0; j < catproduct3.length; j++){
+                catproduct3[j].style.display = "block";
+            }
+            cat_btn_now[0].classList.remove("cat_btn_now");
+            document.getElementById("cat3").closest("li").classList.add("cat_btn_now");
+            break;
+        case '4':
+            for(let i = 0; i < all.length; i++){
+            	all[i].style.display = "none";
+            }
+            for(let j = 0; j < catproduct4.length; j++){
+                catproduct4[j].style.display = "block";
+            }
+            cat_btn_now[0].classList.remove("cat_btn_now");
+            document.getElementById("cat4").closest("li").classList.add("cat_btn_now");
+            break;
+        case '2':
+            for(let i = 0; i < all.length; i++){
+            	all[i].style.display = "none";
+            }
+            for(let j = 0; j < catproduct2.length; j++){
+                catproduct2[j].style.display = "block";
+            }
+            cat_btn_now[0].classList.remove("cat_btn_now");
+            document.getElementById("cat2").closest("li").classList.add("cat_btn_now");
+            break;        
+        }
 });
-numcut.addEventListener("click", function(){
-    if(inputnum.value <= 0){
-        inputnum.value = 0;
-    }else{
-        inputnum.value = parseInt(inputnum.value) - 1;
-    }
-});
 
-
-// 商品分類篩選
+// 商品頁分類篩選
 function btn_cat(btnId, productClass, catClass){
 	var btnCat = document.getElementById(btnId);
 	var allProduct = document.getElementsByClassName(productClass);
 	var catProduct = document.getElementsByClassName(catClass);
 	
-	btnCat.addEventListener("click", function(){
-		// btnCat.style.background="#eea2a4";
+	btnCat.addEventListener("click", function(e){
+        e.preventDefault();
+        document.getElementsByClassName('cat_btn_now')[0].classList.remove("cat_btn_now");
+        btnCat.closest("li").classList.add("cat_btn_now");
 		for(var i = 0; i < allProduct.length; i++){
 			allProduct[i].style.display = "none";
 		}
 		for(var j = 0; j < catProduct.length; j++){
 			catProduct[j].style.display = "block";
-
 		}
 	});
+}
+
+
+// 購買數量
+cart_count();
+function cart_count(){
+    var numplus = document.getElementById("numplus");
+    var numcut = document.getElementById("numcut");
+    var inputnum = document.getElementById("inputnum");
+
+    numplus.addEventListener("click", function(){
+        inputnum.value = parseInt(inputnum.value) + 1;
+    });
+    numcut.addEventListener("click", function(){
+        if(inputnum.value <= 1){
+            inputnum.value = 1;
+        }else{
+            inputnum.value = parseInt(inputnum.value) - 1;
+        }
+    });
+
 }
